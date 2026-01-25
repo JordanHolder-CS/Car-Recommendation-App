13faf6f0195f7a27f84ded0e5763a01e888333b6
JordanHolder-CS <jordangstars@hotmail.com>
2026-01-25 22:44:47 +0000

    Updated default port to use Render's automated .env

--- API/config/server.js
+++ API/config/server.js
@@ -1,12 +1,13 @@
-require("dotenv").config({ path: "./.env" });
+require("dotenv").config({ path: "./config/.env" });
 const express = require("express");
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 const carRoutes = require("../routes/carRoute.js");
+const URL = process.env.HTTPS_URL;
 
 app.use(express.json());
 app.use("/api/car", carRoutes);
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}/api/car`);
+  console.log(`App listening at ${URL}`);
 });
